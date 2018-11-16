82a91866466d53fd0236817083aa7ae166494045
Land Lu <v-luting@microsoft.com>
2018-11-16 14:47:36 +0800

    Add project files.

---
+++ SegmentedControlDemo/SegmentedControlDemo/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SegmentedControlDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_ValueChanged(object sender, SegmentedControl.FormsPlugin.Abstractions.ValueChangedEventArgs e)
+        {
+            switch (e.NewValue)
+            {
+                case 0:
+                    SegContent.Children.Clear();
+                    TestPage test = new TestPage();
+                    SegContent.Children.Add(test.Content);
+                    break;
+                case 1:
+                    SegContent.Children.Clear();
+                    SegContent.Children.Add(new Label() { Text = "tab selected" });
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ SegmentedControlDemo/SegmentedControlDemo/TestPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SegmentedControlDemo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TestPage : ContentPage
+	{
+		public TestPage ()
+		{
+			InitializeComponent ();
+
+            List<string> list = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add("item" + i);
+            }
+            MyListView.ItemsSource = list;
+        }
+	}
+}
